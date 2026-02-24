b56ea72f8019f1f4a44dc8eea3ccfa71b4d8f0cc
Bvanoo <bvanoostveldt@gmail.com>
2026-02-24 09:21:01 +0100

    swagger

--- FamHubBack/Program.cs
+++ FamHubBack/Program.cs
@@ -49,11 +49,9 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseRouting();
 
